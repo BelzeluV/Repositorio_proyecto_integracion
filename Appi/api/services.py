589357a19f628c1102f1082413bbda0b43aebc96
Belzy <101567566+BelzeluV@users.noreply.github.com>
2023-05-10 22:22:36 -0400

    update services.py

--- Appi/api/services.py
+++ Appi/api/services.py
@@ -2,8 +2,9 @@ import requests
 import json
 
 
-def generate_request():
-    response = requests.get("http://localhost:8000/api/").json()
+def generate_request(params):
+    response = requests.get("http://localhost:8000/api/",).json()
+
     return response
 
 
