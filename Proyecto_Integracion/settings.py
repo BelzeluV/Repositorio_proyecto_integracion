03a9cd29c37f5823f6bc4bed2525f09057625535
BelzeluV <101567566+BelzeluV@users.noreply.github.com>
2023-05-15 19:27:39 -0400

    Update settings.py

--- Proyecto_Integracion/settings.py
+++ Proyecto_Integracion/settings.py
@@ -117,7 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+MEDIA_URL = '/media/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_URL = '/static/'     
 STATICFILES_DIRS = [BASE_DIR / "static"]
@@ -128,9 +130,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 """
 Django settings for Proyecto_Integracion project.
 
